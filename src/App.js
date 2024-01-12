3e96c88c9ab4d7568fbbb76478382a361ee06cca
Brian Pena <mr.penabrian@hotmail.com>
2024-01-12 16:01:57 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // Add react-router-dom imports
 
@@ -12,7 +13,7 @@ const appRouter = "REPLACE ME";
 function App() {
   return (
     // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <RouterProvider router="appRouter" />
   );
 }
 
