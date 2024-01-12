69ef93016ceb056522dda921617308c6437dd43d
Brian Pena <mr.penabrian@hotmail.com>
2024-01-12 18:46:54 -0500

    add funcionality to navigate to different pet types

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { getPets } from '../../api/petfinder';
 import Hero from '../../components/hero';
 
-// import useParams
-// import Link
+import { useParams, Link } from 'react-router-dom';
 
 const HomePage = () => {
   const [data, setData] = useState(null);
-  const type = ''; // Fix me!
+  const { type } = useParams();
 
   useEffect(() => {
     async function getPetsData() {
