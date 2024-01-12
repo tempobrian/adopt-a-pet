b0d058cf8f8d46250ddfe1cc72e171429c17d6ee
Brian Pena <mr.penabrian@hotmail.com>
2024-01-12 18:34:28 -0500

    add root route to the app

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
-// Add react-router-dom imports
-
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<Root />} />
+));
 
 function App() {
   return (
-    // replace below with a Router Provider
-    <RouterProvider router="appRouter" />
+    <RouterProvider router={appRouter} />
   );
 }
 
