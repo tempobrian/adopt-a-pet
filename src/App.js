40677fa09848adf78a6486b46bec36d0e20eafee
Brian Pena <mr.penabrian@hotmail.com>
2024-01-15 16:53:45 -0500

    add search functionality

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const appRouter = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={<HomePage />} />
     <Route path=':type' element={<HomePage />} />
     <Route path=':type/:id' element={<PetDetailsPage />} />
+    <Route path='search' element={<SearchPage />} />
   </Route>
 ));
 
